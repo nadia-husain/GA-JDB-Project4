408e0622485c471e95439dcdc08258222972c177
nadia-husain <nadia.neerooz@gmail.com>
2026-05-18 10:51:30 +0300

    class model done

--- src/main/java/com/gym/app/model/Class.java
+++ src/main/java/com/gym/app/model/Class.java
@@ -1,5 +1,52 @@
 package com.gym.app.model;
 
-public class Class {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.gym.app.model.enums.ClassStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "class")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Class {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private LocalDateTime startDate;
+
+    @Column
+    private Integer duration;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private ClassStatus status;
+
+    @CreationTimestamp
+    @Column
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User createdBy;
 }
